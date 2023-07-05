cfa6defd9b0d03089e55a4186887646553cd8614
m-mourouh <mouhamedmourouh1999@gmail.com>
2023-07-05 13:49:56 +0100

    run aptions for deploy were added

--- process.py
+++ process.py
@@ -18,4 +18,5 @@ def process():
     return jsonify({'error' : 'Missing data!'})
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    # app.run(debug = True)
+    app.run(debug = False, host="0.0.0.0")
